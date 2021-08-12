1fe60b2af74a660c3dead5d685ccc3f680710f85
Erika Porto <erikaportoharada@gmail.com>
2021-08-12 16:32:13 -0300

    secao17

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi:true,
+    trailingComma:'all',//adicionando , no final dos objs.
+    singleQuote:true,//adicionando aspas simples
+    printWidth:80,//quebra linha com 
+    tabWidth:2,
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import funcao from './module';
+
+funcao();
